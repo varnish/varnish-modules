56c38f74091181c8d77d03dbe10542b4e9e38f5a
Dridi Boukelmoune <dridi.boukelmoune@gmail.com>
2020-02-18 10:54:32 +0100

    Remove needless forward declarations

--- src/vmod_cookie.c
+++ src/vmod_cookie.c
@@ -51,10 +51,6 @@ enum filter_action {
 	whitelist = 1
 };
 
-vre_t * compile_re(VRT_CTX, VCL_STRING);
-VCL_VOID re_filter(VRT_CTX, struct vmod_priv *, struct vmod_priv *,
-    VCL_STRING, enum filter_action);
-
 static pthread_mutex_t mtx = PTHREAD_MUTEX_INITIALIZER;
 
 struct cookie {
@@ -231,7 +227,7 @@ vmod_get(VRT_CTX, struct vmod_priv *priv, VCL_STRING name)
 }
 
 
-vre_t *
+static vre_t *
 compile_re(VRT_CTX, VCL_STRING expression) {
 	vre_t *vre;
 	const char *error;
@@ -404,9 +400,9 @@ vmod_filter(VRT_CTX, struct vmod_priv *priv, VCL_STRING blacklist_s)
 }
 
 
-VCL_VOID
+static void
 re_filter(VRT_CTX, struct vmod_priv *priv, struct vmod_priv *priv_call,
-		  VCL_STRING expression, enum filter_action mode)
+    VCL_STRING expression, enum filter_action mode)
 {
 	struct vmod_cookie *vcp = cobj_get(priv);
 	struct cookie *current, *safeptr;
